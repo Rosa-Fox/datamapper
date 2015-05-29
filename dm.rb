7931701a05d9ea16ba0f8b4a51fd328658411182
Rosa-Fox <rosafox89@gmail.com>
2015-05-29 12:51:03 +0100

    Added a Pose class and defined post table

--- dm.rb
+++ dm.rb
@@ -17,4 +17,13 @@ class User
   property :updated_at, DateTime
 end
 
+class Post
+  include DataMapper::Resource
+
+  property :slug, String  , key: true, unique_index: true, default: lambda { |resource, prop | resource.title.downcase " ", "-" }
+  property :title, String , required: true
+  property :body, String  , required: true
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
 DataMapper.auto_upgrade!
