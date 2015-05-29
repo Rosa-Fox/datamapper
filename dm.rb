bd4a425c33f85b334edcd5bac9e1607d89dcb25b
Rosa-Fox <rosafox89@gmail.com>
2015-05-29 12:11:40 +0100

    Defined user class and properties

--- dm.rb
+++ dm.rb
@@ -4,3 +4,13 @@ require 'dm-validations'
 require 'dm-migrations'
 
 DataMapper.setup :default, "sqlite://#{Dir.pwd}"/dm.db"
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :username, String
+  property :firstname, String
+  property :lastname, String
+  property :email, String
+end
